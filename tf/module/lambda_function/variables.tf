e37c3b02d8a6d97078a26704692a0ed4b3176ed7
Derek Brown <dmb10vols@gmail.com>
2025-11-20 07:58:36 -0600

    Add code

--- tf/module/lambda_function/variables.tf
+++ tf/module/lambda_function/variables.tf
@@ -5,7 +5,7 @@ variable "description" {}
 variable "filename" {}
 variable "role" {}
 variable "handler" { default = "index.handler" }
-variable "runtime" { default = "nodejs18.x" }
+variable "runtime" { default = "nodejs22.x" }
 variable "publish" { default = "true" }
 variable "timeout" { default = 120 }
 variable "retention_in_days" { default = 3 }
